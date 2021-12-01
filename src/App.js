871c980ee953e0021e91173485dce1a478466e87
Edwu29 <edwardwu10@gmail.com>
2021-11-30 23:20:40 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
+import { useState } from 'react';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [query, setQuery] = useState("");
+  const handleKeyUp = (event) => {
+    setQuery(event.target.value);
+    console.log(event.target.value);
+  }
+  const handleSearch = () => {
+
+  }
   return (
     <div className="App">
-      <Form>
+      <Form onSubmit = {handleSearch}>
         <Form.Group>
           <Form.Label>Search</Form.Label>
-          <Form.Control placeholder="Enter a query"></Form.Control>
+          <Form.Control placeholder="Enter a query" onChange = {handleKeyUp}></Form.Control>
         </Form.Group>
-         <Button variant="primary" type="submit">Submit</Button>
+         <Button variant="primary" type="submit" >Submit</Button>
       </Form>
     </div>
   );
